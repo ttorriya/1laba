6340e7369902c7809db57d9af49ca1cbb5235b9e
Viktoria Barkovskaya <barkovskayaviktoria@gmail.com>
2023-02-16 14:36:38 +0300

    Initial commit

---
+++ src/main/java/SpotifyCloneRunner.java
@@ -0,0 +1,7 @@
+import by.korzun.vanilla_console_app.util.ConsoleApplicationAssembler;
+
+public class SpotifyCloneRunner {
+	public static void main(String[] args) {
+		ConsoleApplicationAssembler.runApplication();
+	}
+}
